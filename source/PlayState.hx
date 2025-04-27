2e8a7128bc1898db0b296378d239ecad03e95aea
Kiovex <kiovex.510@gmail.com>
2025-04-27 18:39:45 +0100

    update 0.1

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,6 +1,5 @@
 package;
 
-import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.addons.editors.ogmo.FlxOgmo3Loader;
 import flixel.tile.FlxTilemap;
@@ -14,8 +13,8 @@ class PlayState extends FlxState
 	{
 		super.create();
 
-		map = new FlxOgmo3Loader("assets/data/second_map.ogmo", "assets/data/second_map.json");
-		walls = map.loadTilemap("assets/images/ground.png", "walls");
+		map = new FlxOgmo3Loader("assets/data/tecer_map.ogmo", "assets/data/tercer_map.json");
+		walls = map.loadTilemap("assets/images/grond_beta.png", "walls");
 		walls.follow();
 		walls.setTileProperties(1, NONE);
 		walls.setTileProperties(2, ANY); 
